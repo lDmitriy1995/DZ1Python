22bd2685ad42372ed0b5e7ee0865d8b7fa9e6f15
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2023-08-16 12:21:14 +0600

    Create DZ1.py

---
+++ DZ1.py
@@ -0,0 +1,53 @@
+#Задание 1
+# Получаем ввод от пользователя
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+num3 = float(input("Введите третье число: "))
+
+# Вычисляем сумму
+sum_result = num1 + num2 + num3
+
+# Вычисляем произведение
+product_result = num1 * num2 * num3
+
+# Выводим результаты
+print("Сумма чисел:", sum_result)
+print("Произведение чисел:", product_result)
+
+
+# #Задание 2
+
+# Получаем ввод от пользователя
+zarplata = float(input("Введите зарплату за месяц: "))
+platеж_kredita = float(input("Введите сумму месячного платежа по кредиту: "))
+zadolzhennost_kommunalka = float(input("Введите задолженность за коммунальные услуги: "))
+
+# Вычисляем оставшуюся сумму
+ostavshayasya_summa = zarplata - platеж_kredita - zadolzhennost_kommunalka
+
+# Выводим результат
+print("Оставшаяся сумма после всех выплат:", ostavshayasya_summa)
+
+
+# #Задание 3
+
+# Получаем ввод от пользователя
+diagonal1 = float(input("Введите длину первой диагонали: "))
+diagonal2 = float(input("Введите длину второй диагонали: "))
+
+# Вычисляем площадь ромба
+area = (diagonal1 * diagonal2) / 2
+
+# Выводим результат
+print("Площадь ромба:", area)
+
+# # #Задание 4
+
+print("To be")
+print("or not")
+print("to be")
+
+
+# # #Задание 5
+quote = "“Life is what happens\nwhen\nyou’re busy making other plans”\nJohn Lennon."
+print(quote)
