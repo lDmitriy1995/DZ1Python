701720b7bbb704a39f77db1bd3b1f85a96324448
lDmitriy1995 <94545009+lDmitriy1995@users.noreply.github.com>
2023-09-29 12:57:25 +0300

    Create DZ22.py

---
+++ DZ22.py
@@ -0,0 +1,120 @@
+#Задание 1
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __le__(self, other):
+        return self.radius <= other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def __ge__(self, other):
+        return self.radius >= other.radius
+
+    def __add__(self, value):
+        return Circle(self.radius + value)
+
+    def __sub__(self, value):
+        return Circle(self.radius - value)
+
+    def __iadd__(self, value):
+        self.radius += value
+        return self
+
+    def __isub__(self, value):
+        self.radius -= value
+        return self
+
+
+#Задание 2
+class Complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return Complex(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imag - other.imag)
+
+    def __mul__(self, other):
+        return Complex(self.real * other.real - self.imag * other.imag,
+                       self.real * other.imag + self.imag * other.real)
+
+    def __truediv__(self, other):
+        denominator = other.real ** 2 + other.imag ** 2
+        real_part = (self.real * other.real + self.imag * other.imag) / denominator
+        imag_part = (self.imag * other.real - self.real * other.imag) / denominator
+        return Complex(real_part, imag_part)
+
+
+
+#Задание 3
+class Airplane:
+    def __init__(self, passengers):
+        self.passengers = passengers
+
+    def __eq__(self, other):
+        return self.passengers == other.passengers
+
+    def __gt__(self, other):
+        return self.passengers > other.passengers
+
+    def __lt__(self, other):
+        return self.passengers < other.passengers
+
+    def __ge__(self, other):
+        return self.passengers >= other.passengers
+
+    def __le__(self, other):
+        return self.passengers <= other.passengers
+
+    def __add__(self, value):
+        return Airplane(self.passengers + value)
+
+    def __sub__(self, value):
+        return Airplane(self.passengers - value)
+
+    def __iadd__(self, value):
+        self.passengers += value
+        return self
+
+    def __isub__(self, value):
+        self.passengers -= value
+        return self
+
+
+#Задание 4
+class Flat:
+    def __init__(self, area, price):
+        self.area = area
+        self.price = price
+
+    def __eq__(self, other):
+        return self.area == other.area
+
+    def __ne__(self, other):
+        return self.area != other.area
+
+    def __lt__(self, other):
+        return self.price < other.price
+
+    def __le__(self, other):
+        return self.price <= other.price
+
+    def __gt__(self, other):
+        return self.price > other.price
+
+    def __ge__(self, other):
+        return self.price >= other.price
+
+
+
